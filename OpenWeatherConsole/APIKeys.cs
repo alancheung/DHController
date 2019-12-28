93ac5dbd152287dd81d702d43c50eb4ea69a7dc5
Alan Cheung <cheung@rdacorp.com>
2019-12-27 21:14:42 -0500

    Add new static class for API keys.

---
+++ OpenWeatherConsole/APIKeys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWeatherConsole
+{
+    public static partial class APIKeys
+    {
+        public static readonly string OPEN_WEATHER_KEY = string.Empty;
+        public static readonly string BALTIMORE_CITY = string.Empty;
+    }
+}
