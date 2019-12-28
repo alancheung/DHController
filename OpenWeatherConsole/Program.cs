9a76f1943ccdf76f1d49cf0b8adae5ae3cbb45be
Alan Cheung <cheung@rdacorp.com>
2019-12-27 21:34:03 -0500

    Add API for open weather.

--- OpenWeatherConsole/Program.cs
+++ OpenWeatherConsole/Program.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+﻿using OpenWeatherMap;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -9,20 +9,29 @@ using System.Threading.Tasks;
 
 namespace OpenWeatherConsole
 {
-    class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            object untrackedConfiguration = ConfigurationManager.AppSettings;
+            //Int32.TryParse(APIKeys.BALTIMORE_CITY, out int baltimoreKey);
+            //SingleResult<CurrentWeatherResult> currentWeather = CurrentWeather.GetByCityId(baltimoreKey);
+
+            Int32.TryParse(APIKeys.BALTIMORE_CITY, out int baltimoreKey);
+            OpenWeatherMapClient client = new OpenWeatherMapClient(APIKeys.OPEN_WEATHER_KEY);
+            Task<CurrentWeatherResponse> getter = client.CurrentWeather.GetByCityId(baltimoreKey);
+            getter.Wait(10000);
+            CurrentWeatherResponse currentWeather = getter.Result;
+
+
 
             //using (HttpClient client = new HttpClient())
             //{
-            //    var response = client.GetAsync(new Uri(String.Format("https://api.openweathermap.org/data/2.5/weather?id={0}&appid={1}", BALTIMORE, KEY))).Result;
+            //    var response = client.GetAsync(new Uri(String.Format("https://api.openweathermap.org/data/2.5/weather?id={0}&appid={1}", APIKeys.BALTIMORE_CITY, APIKeys.OPEN_WEATHER_KEY))).Result;
             //    Console.WriteLine($"Successful: {response.IsSuccessStatusCode}");
 
             //    Task<string> test = response.Content.ReadAsStringAsync();
             //    test.Wait();
-            //    dynamic data = JsonConvert.DeserializeObject<dynamic>(test.Result);
+            //    CurrentWeatherResult data = JsonConvert.DeserializeObject<CurrentWeatherResult>(test.Result);
             //}
 
             Console.ReadKey();
