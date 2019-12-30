10f29e4b2ae6468412f3ad4b353d863ff700d15b
Alan Cheung <cheung@rdacorp.com>
2019-12-29 23:09:52 -0500

    Rename as DHCService

---
+++ DHCService/DHCService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DHCService
+{
+    public partial class DHCService : ServiceBase
+    {
+        public DHCService()
+        {
+            InitializeComponent();
+        }
+
+        public void DebugStart(string[] args)
+        {
+            OnStart(args);
+            Console.Read();
+            OnStop();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+    }
+}
---
+++ DHCService/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DHCService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            if (Environment.UserInteractive)
+            {
+                DHCService service1 = new DHCService();
+                service1.DebugStart(args);
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                    new DHCService()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+
+
+    }
+}
