c0051e774c58a8c23b3fed6ca27ca19ce6a03825
Alan Cheung <cheung@rdacorp.com>
2020-01-03 22:03:46 -0500

    Clarify comments/variables to improve readability.

--- PyWandTracker/wandTracker.py
+++ PyWandTracker/wandTracker.py
@@ -13,83 +13,61 @@ camera.set(4, 480)
 
 thresh = 40
 
-def detectShape(c):
-    # initialize the shape name and approximate the contour
-    shape = "unidentified"
-    peri = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.04 * peri, True)
-    # if the shape is a triangle, it will have 3 vertices
-    if len(approx) == 3:
-        shape = "triangle"
-
-        # if the shape has 4 vertices, it is either a square or
-        # a rectangle
-    elif len(approx) == 4:
-        # compute the bounding box of the contour and use the
-        # bounding box to compute the aspect ratio
-        (x, y, w, h) = cv2.boundingRect(approx)
-        ar = w / float(h)
-
-        # a square will have an aspect ratio that is approximately
-        # equal to one, otherwise, the shape is a rectangle
-        shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
- 
-    # if the shape is a pentagon, it will have 5 vertices
-    elif len(approx) == 5:
-        shape = "pentagon"
-    
-    # otherwise, we assume the shape is a circle
-    else:
-        shape = "circle"
-
-    # return the name of the shape
-    return shape
+def detectSpell():
+    return 'idk'
 
 while 1:
+    # read from camera
     (grabbed, frame) = camera.read()
     frame = imutils.rotate(frame, angle=180)
-
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # TODO determine if this completely necessary
     cv2.equalizeHist(frame_gray)
+
+    # resize image for faster processing.
     #frame_gray = cv2.resize(frame_gray, (120, 120), interpolation = cv2.INTER_CUBIC)
 
-    # find the point by looking for pixels >40
+    # find the point by looking for pixels >threshold
     th, frame_gray = cv2.threshold(frame_gray, thresh, 255, cv2.THRESH_BINARY)
 
-    # make the dot bigger
-    frame_gray = cv2.dilate(frame_gray, kernel, iterations = 3)
+    # At least 1 pass is needed to create centroid for recognition
+    # This approach may not be needed if Hough circles are used.
+    frame_gray = cv2.dilate(frame_gray, kernel, iterations = 1)
  
-    # find contours in the mask and initialize the current
-	# (x, y) center of the ball
+    # find contours in the mask
+    # countours meaning the binary area (aka in our case, the white dot).
     cnts = cv2.findContours(frame_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     center = None
  
 	# only proceed if at least one contour was found
     if len(cnts) > 0:
-		# find the largest contour in the mask, then use
-		# it to compute the minimum enclosing circle and
-		# centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        # TODO maybe should change this to assume the cnt that is closest to center of frame is the most applicable instead of max.
+		# find the most applicable contour in the mask (in this case the largest), then use it to compute the minimum enclosing circle and if it matches a known spell.
+        mostLikelyWandTip = max(cnts, key=cv2.contourArea)
 
-        # find spell?????
-        shape = detectShape(c)
-        print shape
+        # find bounds of circle in order to show on screen. Not applicable in this sense
+        # ((x, y), radius) = cv2.minEnclosingCircle(c)
+
+        # Moments help find centers of points
+        # https://www.learnopencv.com/find-center-of-blob-centroid-using-opencv-cpp-python/
+        M = cv2.moments(mostLikelyWandTip)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
 	# update the points queue
     pts.appendleft(center)
 
-    # loop over the set of tracked points
+    # loop over the set of tracked points so that we can draw a line for human eyes.
+    # TODO control view port/this extra work by argument
     for i in range(1, len(pts)):
         # if either of the tracked points are None, ignore them
         if pts[i - 1] is None or pts[i] is None:
             continue
 
-        # otherwise, compute the thickness of the line and
-        # draw the connecting lines
-        cv2.line(frame_gray, pts[i - 1], pts[i], (255, 0, 0), thickness = 10)
+        # otherwise, compute the thickness of the line and draw the connecting lines
+        cv2.line(frame_gray, pts[i - 1], pts[i], (255, 0, 0), thickness = 5)
+
+    
 
     # loop and show frame
     cv2.imshow('raw', frame)
