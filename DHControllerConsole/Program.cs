4994091581a934a2c921eafc589b8e212405266d
Alan Cheung <cheung@rdacorp.com>
2020-01-11 21:49:21 -0500

    Add initial logic to control turning on and off monitors.

--- DHControllerConsole/Program.cs
+++ DHControllerConsole/Program.cs
@@ -15,73 +15,34 @@ namespace DHControllerConsole
 {
     public class Program
     {
-        static CountdownEvent countdown;
-        static int upCount = 0;
-        static object lockObj = new object();
-        const bool resolveNames = true;
-
         public static void Main(string[] args)
         {
-            countdown = new CountdownEvent(1);
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            string ipBase = "10.0.0.";
-            string ip = $"Kellys-iPhone";
-
-            Ping p = new Ping();
-            p.PingCompleted += new PingCompletedEventHandler(p_PingCompleted);
-            countdown.AddCount();
-            p.SendAsync(ip, 1500, ip);
-
-            //for (int i = 1; i < 255; i++)
-            //{
-            //    string ip = ipBase + i.ToString();
-
-            //    Ping p = new Ping();
-            //    p.PingCompleted += new PingCompletedEventHandler(p_PingCompleted);
-            //    countdown.AddCount();
-            //    p.SendAsync(ip, 1000, ip);
-            //}
-            countdown.Signal();
-            countdown.Wait();
-            sw.Stop();
-            Console.WriteLine("Took {0} milliseconds. {1} hosts active.", sw.ElapsedMilliseconds, upCount);
             Console.ReadLine();
-        }
+            MonitorHelper.TurnOff();
+            Console.ReadKey();
+            MonitorHelper.TurnOn();
 
-        static void p_PingCompleted(object sender, PingCompletedEventArgs e)
-        {
-            string ip = (string)e.UserState;
-            if (e.Reply != null && e.Reply.Status == IPStatus.Success)
-            {
-                if (resolveNames)
-                {
-                    string name;
-                    try
-                    {
-                        IPHostEntry hostEntry = Dns.GetHostEntry(ip);
-                        name = hostEntry.HostName;
-                    }
-                    catch (SocketException ex)
-                    {
-                        name = "?";
-                    }
-                    Console.WriteLine("{0} ({1}) is up: ({2} ms)", ip, name, e.Reply.RoundtripTime);
-                }
-                else
-                {
-                    Console.WriteLine("{0} is up: ({1} ms)", ip, e.Reply.RoundtripTime);
-                }
-                lock (lockObj)
-                {
-                    upCount++;
-                }
-            }
-            else if (e.Reply == null)
-            {
-                Console.WriteLine("Pinging {0} failed. (Null Reply object?)", ip);
-            }
-            countdown.Signal();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
         }
     }
 }
