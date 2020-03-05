901ae5eb16bedf7dded4f5be76f4095eef78cc1f
Alan Cheung <cheung@rdacorp.com>
2020-03-04 20:09:53 -0500

    Move all files from old directory.

---
+++ Python.Repository/TestCode/lifxTester.py
@@ -0,0 +1,27 @@
+from lifxlan import LifxLAN
+from time import sleep
+import sys
+
+lifx = LifxLAN(7)
+officeLightGroup = lifx.get_devices_by_group("Office")
+officeLights = officeLightGroup.get_device_list()
+officeOne = next(filter(lambda l: l.get_label() == "Office One", officeLights), None)
+officeTwo = next(filter(lambda l: l.get_label() == "Office Two", officeLights), None)
+officeThree = next(filter(lambda l: l.get_label() == "Office Three", officeLights), None)
+
+def lightOnSequence():
+    print("One on!")
+    officeOne.set_power("on", duration=5000)
+    sleep(1)
+    print("Two on!")
+    officeTwo.set_power("on", duration=4000)
+    sleep(1)
+    print("Three on!")
+    officeThree.set_power("on", duration=3000)
+    sleep(1)
+
+
+if len(officeLights) < 3:
+    print(f"Did not discover all office lights! ({len(officeLights)} of 3)")
+    sys.exit(-1)
+
