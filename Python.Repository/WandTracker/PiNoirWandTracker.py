d9958611c8a664644103feb559c02da1d625c0ad
Alan Cheung <cheung@rdacorp.com>
2020-04-19 11:26:17 -0400

    Fix IDE warnings about whitespace.

--- Python.Repository/WandTracker/PiNoirWandTracker.py
+++ Python.Repository/WandTracker/PiNoirWandTracker.py
@@ -32,7 +32,7 @@ pts = deque(maxlen=args["buffer"])
 # to the webcam
 if not args.get("video", False):
 	camera = cv2.VideoCapture(0)
- 
+
 # otherwise, grab a reference to the video file
 else:
 	camera = cv2.VideoCapture(args["video"])
@@ -41,19 +41,19 @@ else:
 while True:
 	# grab the current frame
 	(grabbed, frame) = camera.read()
- 
+
 	# if we are viewing a video and we did not grab a frame,
 	# then we have reached the end of the video
 	if args.get("video") and not grabbed:
 		break
- 
+
 	# resize the frame, inverted ("vertical flip" w/ 180degrees),
 	# blur it, and convert it to the HSV color space
 	frame = imutils.resize(frame, width=600)
 	frame = imutils.rotate(frame, angle=180)
 	# blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
- 
+
 	# construct a mask for the color "green", then perform
 	# a series of dilations and erosions to remove any small
 	# blobs left in the mask
@@ -66,7 +66,7 @@ while True:
 	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)[-2]
 	center = None
- 
+
 	# only proceed if at least one contour was found
 	if len(cnts) > 0:
 		# find the largest contour in the mask, then use
@@ -76,7 +76,7 @@ while True:
 		((x, y), radius) = cv2.minEnclosingCircle(c)
 		M = cv2.moments(c)
 		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
- 
+
 		# only proceed if the radius meets a minimum size
 		if radius > 10:
 			# draw the circle and centroid on the frame,
@@ -84,7 +84,7 @@ while True:
 			cv2.circle(frame, (int(x), int(y)), int(radius),
 				(0, 255, 255), 2)
 			cv2.circle(frame, center, 5, (0, 0, 255), -1)
- 
+
 	# update the points queue
 	pts.appendleft(center)
 	
@@ -94,20 +94,20 @@ while True:
 		# them
 		if pts[i - 1] is None or pts[i] is None:
 			continue
- 
+
 		# otherwise, compute the thickness of the line and
 		# draw the connecting lines
 		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
 		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
- 
+
 	# show the frame to our screen
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
- 
+
 	# if the 'q' key is pressed, stop the loop
 	if key == ord("q"):
 		break
- 
+
 # cleanup the camera and close any open windows
 camera.release()
 cv2.destroyAllWindows()
--- Python.Repository/WandTracker/WandTracker.Simple/Tracker.py
+++ Python.Repository/WandTracker/WandTracker.Simple/Tracker.py
@@ -1,5 +1,4 @@
 from collections import deque
-import ConsoleLogger
 import numpy as np
 import cv2
 import imutils
